c33a0b2bff5742ed1928e950c53065b0327de034
userZheng686 <1511019025@qq.com>
2020-08-19 16:56:51 +0800

    构建项目，配置全局样式，路由，elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+    123
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
